cf4581121718555c5ca25260d79962a970b3240b
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-13 10:17:27 +0530

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,10 +4,11 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+  // conn.sendFile(m.chat, json.url, '', 'Istri kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+  await conn.sendButtonImg(m.chat, await(await fetch(json.url)).buffer(), 'Why is your girlfriend a cartoon?', '© MilfBOT', 'Next', '.waifu', m)
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(waifu)$/i
 
 module.exports = handler
