4d02148d440c271eafefc62c7ec4b1f97d5b6c5e
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-13 12:40:33 +0530

    Update snackvideo.js

--- plugins/snackvideo.js
+++ plugins/snackvideo.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'uhm.. url nya mana?'
+  if (!args[0]) throw 'uhm.. Send snackvideo URL?'
   global.API('xteam', '/dl/', {
     url: args[0]
   }, 'APIKEY')
   conn.sendFile(m.chat, undefined, '', '', m)
 }
-handler.help = [].map(v => v + ' <url>')
+handler.help = ['snackvideo'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^\x00s$/i
