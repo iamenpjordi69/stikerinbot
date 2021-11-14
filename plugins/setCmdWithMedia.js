ea3447e629fa7845e262ab4863241cec35c35164
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 07:32:09 +0530

    Update setCmdWithMedia.js

--- plugins/setCmdWithMedia.js
+++ plugins/setCmdWithMedia.js
@@ -1,11 +1,11 @@
 module.exports = Object.assign(async function handler(m, { text }) {
     global.db.data.sticker = global.db.data.sticker || {}
-    if (!m.quoted) throw 'balas stikernya!'
+    if (!m.quoted) throw 'reply to a sticker!'
     if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
-    if (!text) throw `uhm.. teksnya mana?`
+    if (!text) throw `uhm.. where is the text?`
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (sticker[hash] && sticker[hash].locked) throw 'kamu tidak memiliki izin untuk mengubah perintah stiker ini'
+    if (sticker[hash] && sticker[hash].locked) throw 'you dont have permission to change this sticker order'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
@@ -15,7 +15,7 @@ module.exports = Object.assign(async function handler(m, { text }) {
     }
     m.reply(`Berhasil!`)
 }, {
-    help: ['cmd'].map(v => 'set' + v + ' <teks>'),
+    help: ['cmd'].map(v => 'set' + v + ' <text>'),
     tags: ['database'],
     command: ['setcmd']
 })
