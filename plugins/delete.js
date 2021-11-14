b555fa885db02e7474c53ea5e0adf8168fa607bd
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 23:08:25 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,8 +2,8 @@ let handler = function (m) {
   if (!m.quoted) throw false
   let { chat, fromMe, id, isBaileys } = m.quoted
   if (!fromMe) throw false
-  if (/Stikerin Broadcast/i.test(m.quoted.text)) throw 'Tidak bisa menghapus pesan broadcast!'
-  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+  if (/Stikerin Broadcast/i.test(m.quoted.text)) throw 'Cannot delete broadcast messages!'
+  if (!isBaileys) throw 'The message was not sent by a bot!'
   this.deleteMessage(chat, {
     fromMe,
     id,
@@ -12,6 +12,7 @@ let handler = function (m) {
 }
 handler.help = ['del', 'delete']
 handler.tags = ['info']
+handler.admin = true
 
 handler.command = /^del(ete)?$/i
 
