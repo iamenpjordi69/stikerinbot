1c09ca2bf9b8e7cf3541389833de27c96ef7ff64
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 18:48:38 +0530

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -2,15 +2,15 @@ const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} stikerinbot`
-  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
+  if (!args[0]) throw `uhm.. sennd a username?\n\nExample:\n\n${usedPrefix + command} nsutjordi`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `wrong username`
 
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
     let json = JSON.parse(igs)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© stikerin', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© MilfBOT', m)
 
   })
 
