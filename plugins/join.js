3d040090568578d4834bd33f1083237b033dae05
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 18:13:55 +0530

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -2,7 +2,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link Salah'
+    if (!code) throw 'Invalid WhatsApp Group Link'
     let res = await conn.acceptInvite(code)
     m.reply(`Berhasil join grup ${res.gid}`).then(() => {
         var jumlahHari = 86400000 * 0.5
@@ -11,9 +11,9 @@ let handler = async (m, { conn, text, usedPrefix }) => {
         else global.db.data.chats[res.gid].expired = now + jumlahHari
     })
     await conn.sendButton(res.gid, `
-*${conn.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, *${conn.user.name}* diundang oleh @${m.sender.split`@`[0]}
+*${conn.user.name}* is a whatsapp bot built with Nodejs and Typescript, *${conn.user.name}* invited by @${m.sender.split`@`[0]}
     
-ketik *${usedPrefix}menu* untuk melihat daftar perintah`.trim(), '© stikerin', 'Menu', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
+Type *${usedPrefix}menu* to see a list of commands`.trim(), '© MilfBOT', 'Menu', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['']
