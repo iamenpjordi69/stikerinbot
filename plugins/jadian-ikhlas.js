5a241a92e4bf3f8a09438f035d44f91b909f8447
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 18:43:08 +0530

    Update jadian-ikhlas.js

--- plugins/jadian-ikhlas.js
+++ plugins/jadian-ikhlas.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text }) => {
-  if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `Kamu sedang tidak menembak siapapun!`, m)
-  if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `Kamu telah berpacaran dengan @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: {
+  if (global.db.data.users[m.sender].pasangan == "") return conn.reply(m.chat, `You're not shooting anyone!`, m)
+  if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `You have been dating @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
-  conn.reply(m.chat, `Kamu sudah mengikhlaskan @${global.db.data.users[m.sender].pasangan.split('@')[0]} karena dia tidak memberikan jawaban diterima atau ditolak`, m, {contextInfo: {
+  conn.reply(m.chat, `You've made it up @${global.db.data.users[m.sender].pasangan.split('@')[0]} because he didn't give an answer accepted or rejected`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
   global.db.data.users[m.sender].pasangan = ""
