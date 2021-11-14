35d6721bfd36f382e20318376c1f8b70a95de87a
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 23:40:50 +0530

    Update asahotak_hint.js

--- plugins/asahotak_hint.js
+++ plugins/asahotak_hint.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.asahotak)) throw false
     let json = conn.asahotak[id][1]
-    let clue = json.jawaban.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.asahotak[id][0])
+    let clue = json.answer.replace(/[AIUEOaiueo]/g, '_')
+    conn.reply(m.chat, '```' + clue + '```\nAnswer the question, not this message', conn.asahotak[id][0])
 }
 handler.command = /^ao$/i
 handler.limit = true
