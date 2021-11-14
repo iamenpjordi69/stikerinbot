978780b9c47f03c25bbe2913ce453be11e2c7634
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 07:31:03 +0530

    Update setProfileGroup.js

--- plugins/setProfileGroup.js
+++ plugins/setProfileGroup.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak didukung`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} unsupporetd format`
     if (/image/.test(mime)) {
         let img = await q.download()
-        if (!img) throw 'Gambar tidak ditemukan'
+        if (!img) throw 'Image not found'
         await conn.updateProfilePicture(m.chat, img)
-    } else throw `kirim/balas gambar dengan perintah *${usedPrefix + command}*`
+    } else throw `send/reply image with command *${usedPrefix + command}*`
 }
 handler.help = ['setpp']
 handler.tags = ['group']
