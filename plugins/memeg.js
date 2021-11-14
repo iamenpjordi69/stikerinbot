4eae9d4a34f88351e07bce933807e7258a69089a
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 13:52:39 +0530

    Update memeg.js

--- plugins/memeg.js
+++ plugins/memeg.js
@@ -1,19 +1,19 @@
 const uploadFile = require('../lib/uploadFile')
 const uploadImage = require('../lib/uploadImage')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. teksnya mana?\n\n${usedPrefix + command} <teks atas>|<teks bawah>`
+  if (!text) throw `uhm.. Where is the text?\n\n${usedPrefix + command} <top text>|<bottom text>`
   let [t1, t2] = text.split`|`
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw `Unknown Mimetype`
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak didukung`
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} format not supported`
   let img = await q.download()
   let link = await uploadImage(img).catch(e => uploadFile(img))
   conn.sendFile(m.chat, global.API('https://api.memegen.link', `/images/custom/${encodeURIComponent(t1 ? t1 : '_')}/${encodeURIComponent(t2 ? t2 : '_')}.png`, {
     background: link
-  }), 'meme.png', '© stikerin', m)
+  }), 'meme.png', '© MilfBOT', m)
 }
-handler.help = ['mememaker'].map(v => v + ' <teks atas>|<teks bawah>')
+handler.help = ['mememaker'].map(v => v + ' <top text>|<bottom text>')
 handler.tags = ['tools']
 handler.command = /^(meme(maker|g))$/i
 
