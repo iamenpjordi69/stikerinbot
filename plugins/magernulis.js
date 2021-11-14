af2849221cb4009443a427800c0831885198188d
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 13:57:42 +0530

    Update magernulis.js

--- plugins/magernulis.js
+++ plugins/magernulis.js
@@ -1,17 +1,18 @@
-let fetch = require('node-fetch')
-let handler = async (m, { command, conn, text }) => {
+// Magernulis By MFarelS:V
+// Code by DrawlNag
+// Recode by Nurutomo :V
+let handler  = async (m, { command, conn, text }) => {
   let id = (command.match(/[1-6]$/) || [])[0] || ''
-  let url = global.API('xteam', '/magernulis' + id, {
+  await conn.sendFile(m.chat, global.API('xteam', '/magernulis' + id, {
     text,
     nama: conn.getName(m.sender),
     kelas: ' '
-  }, 'APIKEY')
-  await conn.sendFile(m.chat, url, 'nulis.jpg', '', m, false, { thumbnail: await (await fetch(url)).buffer() })
+  }, 'APIKEY'), 'nulis.jpg', 'Nahh it\'s done...', m)
 }
-handler.help = new Array(6).fill('magernulis').map((v, i) => v + (i + 1) + ' <teks>')
+handler.help = new Array(6).fill('lazywrite').map((v, i) => v + (i + 1) + ' <text>')
 handler.tags = ['nulis']
 
-handler.command = /^magernulis[1-6]?$/i
+handler.command = /^lazywrite[1-6]?$/i
 
 handler.limit = true
 
