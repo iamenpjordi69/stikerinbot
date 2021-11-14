3de616231b21965450d1916c17b89654637bc2d4
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 12:57:37 +0530

    Update nsfwwaifu.js

--- plugins/nsfwwaifu.js
+++ plugins/nsfwwaifu.js
@@ -1,6 +1,6 @@
 let fetch = require("node-fetch");
 let handler = async (m, { conn }) => {
-  if (!db.data.settings.nsfw) throw "Mode NSFW tidak aktif";
+  if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
   let res = await fetch(global.API("https://api.waifu.pics/", "/nsfw/waifu"));
   if (!res.ok) throw await `${res.status} ${res.statusText}`;
   let json = await res.json();
@@ -8,14 +8,14 @@ let handler = async (m, { conn }) => {
     conn.sendFile(
       m.chat,
       json.url,
-      "bokepkartun",
-      "sange kok sama kartun, stress...",
+      "cartoon porn",
+      "how awesome is the cartoon, stress...",
       m
     );
   else throw json;
 };
 handler.help = ["nsfwwaifu", "waifunsfw"];
-handler.tags = ["fun"];
+handler.tags = ["anime"];
 
 handler.command = /^(nsfwwaifu|waifunsfw)$/i;
 
