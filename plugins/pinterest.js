db851c66ed12f398805cdd17f6cdac4ae1f6b9e6
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 12:29:53 +0530

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} logo`
+  if (!text) throw `uhm.. send a query?\n\nExample:\n${usedPrefix + command}  pubg logo`
   let res = await fetch(global.API('zeks', '/api/pinimg', {
     q: text
   }, 'apikey'))
@@ -8,9 +8,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let json = await res.json()
   if (!json.status) throw json
   let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', '© stikerin', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
+  conn.sendFile(m.chat, pint, '', '© MilfBOT', m, 0, { thumbnail: await (await fetch(pint)).buffer() })
 }
-handler.help = ['pinterest <pencarian>']
+handler.help = ['pinterest <search>']
 handler.tags = ['internet']
 handler.command = /^(pint(erest)?)$/i
 
