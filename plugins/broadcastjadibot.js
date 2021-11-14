80be480da4434e6f32b1d15800bb82f7da269254
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 23:24:14 +0530

    Update broadcastjadibot.js

--- plugins/broadcastjadibot.js
+++ plugins/broadcastjadibot.js
@@ -3,16 +3,16 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Stikerin Broadcast 〕\n\n' + teks)
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 MilfBOT Broadcast 〕\n\n' + teks)
   for (let id of users) {
     await delay(1500)
     await conn.copyNForward(id, content, true)
   }
-  conn.reply(m.chat, `_Berhasil mengirim broadcast ke ${users.length} nomor yang jadi bot_
+  conn.reply(m.chat, `_Successfully broadcast to ${users.length} the bot numbers_
 ${users.map(v => 'wa.me/' + v.replace(/[^0-9]/g, '') + `?text=${encodeURIComponent(usedPrefix)}menu`).join('\n')}
-\nestimasi selesai ${users.length * 1.5} detik`.trim(), m)
+\nEstimated time for completion ${users.length * 1.5} seconds`.trim(), m)
 }
-handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <teks>')
+handler.help = ['broadcastjadibot', 'bcbot'].map(v => v + ' <text>')
 handler.tags = ['host']
 handler.command = /^(broadcast|bc)(jadi)?bot$/i
 handler.owner = true
