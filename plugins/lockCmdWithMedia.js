4cd3daa6ccfc0475b21cba5c97040ced0c161114
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 14:01:26 +0530

    Update lockCmdWithMedia.js

--- plugins/lockCmdWithMedia.js
+++ plugins/lockCmdWithMedia.js
@@ -3,9 +3,9 @@ module.exports = Object.assign(async function handler(m, { command }) {
     if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'Hash tidak ditemukan di database'
+    if (!(hash in sticker)) throw 'Hash not found in database'
     sticker[hash].locked = !/^un/i.test(command)
-    m.reply('Berhasil!')
+    m.reply('Success!')
 }, {
     rowner: true,
     help: ['un', ''].map(v => v + 'lockcmd'),
