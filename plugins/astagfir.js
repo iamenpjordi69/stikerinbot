da8f8f26fc3ced9c101b99ae3bae3664af8f651f
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 23:37:10 +0530

    Update astagfir.js

--- plugins/astagfir.js
+++ plugins/astagfir.js
@@ -1,9 +1,9 @@
 let handler = async (m) => {
     let user = db.data.users[m.sender]
-    if (user.warning == 0) throw 'Kamu tidak memiliki warning!'
+    if (user.warning == 0) throw 'You have no warning!'
 
     let waktu = user.lastIstigfar + 180000
-    if (new Date - user.lastIstigfar < 180000) throw `Kamu bisa menggunakan perintah ini lagi setelah ${msToTime(waktu - new Date())}`
+    if (new Date - user.lastIstigfar < 180000) throw `You can use this command again after ${msToTime(waktu - new Date())}`
     user.warning -= 1
     m.reply(`Warning: ${user.warning} / 5`)
     user.lastIstigfar = new Date * 1
@@ -24,5 +24,5 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? "0" + minutes : minutes
     seconds = (seconds < 10) ? "0" + seconds : seconds
 
-    return minutes + " menit " + seconds + " detik"
+    return minutes + "minutes" + seconds + "seconds"
 }
