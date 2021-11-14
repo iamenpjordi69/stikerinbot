7cda40b3aedba1909c8e15357c2617f897b49451
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 10:16:57 +0530

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,35 +2,34 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  if (user.registered === true) throw `You are already registered\nWant to re-register? ${usedPrefix}unreg <SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Example:\n*${usedPrefix + command} name.age*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Name cannot be empty (Alphanumeric)'
+  if (!age) throw 'Age cant be empty (Digit)'
   age = parseInt(age)
-  if (age > 70) throw 'Umur terlalu tua'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 70) throw 'Age too old'
+  if (age < 5) throw 'Babies can type according to the DOB format ._.'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Successfully Registered!
 
 ┌─〔 Info 〕
-├ Nama: ${name}
-├ Umur: ${age} tahun
+├ Name: ${name}
+├ Age: ${age} years
 ├ SN: ${sn}
 └────
 
-simpan/bintangi pesan ini karena SN (Serial Number) digunaan untuk daftar ulang
+save/star this message because SN (Serial Number) is used for re-registration
 `.trim())
 }
-handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <name>.<age>')
 handler.tags = ['xp']
 
 handler.command = /^(daftar|reg(ister)?)$/i
 
 module.exports = handler
-
