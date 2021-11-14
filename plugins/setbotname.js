bf77f12099df18cc6cfa174bfe0228518b2eefc9
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 07:29:01 +0530

    Update setbotname.js

--- plugins/setbotname.js
+++ plugins/setbotname.js
@@ -1,15 +1,15 @@
 // NurNurz
 let handler = async (m, { conn, text }) => {
-  if (!text) throw `uhm.. teksnya mana?`
+  if (!text) throw `uhm.. where is the text?`
   try {
     await conn.updateProfileName(text)
-    m.reply('Berhasil!')
+    m.reply('Bot name changed Successfully!')
   } catch (e) {
     console.log(e)
     throw `Error`
   }
 }
-handler.help = ['setbotname <teks>']
+handler.help = ['setbotname <text>']
 handler.tags = ['owner']
 handler.command = /^(setbotname)$/i
 handler.owner = true
