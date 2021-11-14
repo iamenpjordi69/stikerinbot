20ab3989a7301c0a3cf65844cca5e25e23ffe1a8
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 18:59:35 +0530

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -4,13 +4,13 @@ let gis = promisify(_gis)
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} pisang`
+  if (!text) throw `uhm.. What are you looking for?\n\nExample:\n${usedPrefix + command} Dani Daniels`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
   if (!url) throw '404 Not Found'
   conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
-handler.help = ['gimage <pencarian>', 'image <pencarian>']
+handler.help = ['gimage <search>', 'image <search>']
 handler.tags = ['internet']
 handler.command = /^(g?image)$/i
 
