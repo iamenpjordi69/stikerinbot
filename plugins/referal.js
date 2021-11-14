90dc38fadeeb92a2c21042bb00600fad895f6e3d
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 10:14:18 +0530

    Update referal.js

--- plugins/referal.js
+++ plugins/referal.js
@@ -1,65 +1,35 @@
-const crypto = require('crypto')
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix, command }) {
+  let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `You are already registered\nWant to re-register? ${usedPrefix}unreg <SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Example:\n*${usedPrefix + command} name.age*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw 'Name cannot be empty (Alphanumeric)'
+  if (!age) throw 'Age cant be empty (Digit)'
+  age = parseInt(age)
+  if (age > 70) throw 'Age too old'
+  if (age < 5) throw 'Babies can type according to the DOB format ._.'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  m.reply(`
+Successfully Registered!
 
-const xp_first_time = 2500
-const xp_link_creator = 15000
-const xp_bonus = {
-    5: 40000,
-   10: 100000,
-   20: 250000,
-   50: 1000000,
-  100: 10000000,
-}
+┌─〔 Info 〕
+├ Name: ${name}
+├ Age: ${age} years
+├ SN: ${sn}
+└────
 
-let handler = async (m, { conn, usedPrefix, text }) => {
-  let users = global.db.data.users
-  if (text) {
-    if ('ref_count' in users[m.sender]) throw 'Tidak bisa menggunakan kode referal!'
-    let link_creator = (Object.entries(users).find(([, { ref_code }]) => ref_code === text.trim()) || [])[0]
-    if (!link_creator) throw 'Kode referal tidak valid'
-    let count = users[link_creator].ref_count++
-    let extra = xp_bonus[count] || 0
-    users[link_creator].exp += xp_link_creator + extra
-    users[m.sender].exp += xp_first_time
-    users[m.sender].ref_count = 0
-    m.reply(`
-Selamat!
-+${xp_first_time} XP
+save/star this message because SN (Serial Number) is used for re-registration
 `.trim())
-    m.reply(`
-Seseorang telah menggunakan kode referal kamu
-+${xp_link_creator + extra} XP
-`.trim(), link_creator)
-  } else {
-    let code = users[m.sender].ref_code = users[m.sender].ref_code || new Array(11).fill().map(() => [...'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'][crypto.randomInt(62)]).join('')
-    users[m.sender].ref_count = users[m.sender].ref_count ? users[m.sender].ref_count : 0
-    let command_text = `${usedPrefix}ref ${code}`
-    let command_link = `wa.me/${conn.user.jid.split('@')[0]}?text=${encodeURIComponent(command_text)}`
-    let share_text = `
-Dapatkan ${xp_first_time} XP untuk yang menggunakan link/kode referal dibawah ini
-
-Referal Code: *${code}*
-
-${command_link}
-`.trim()
-    m.reply(`
-Dapatkan ${xp_link_creator} XP untuk setiap pengguna baru yang menggunakan kode referal kamu
-${users[m.sender].ref_count} orang telah menggunakan kode referal kamu
-
-Kode referal kamu: ${code}
-
-Bagikan link kepada teman: ${command_link}
-
-atau kirim pesan kepada teman wa.me/?text=${encodeURIComponent(share_text)}
-
-${Object.entries(xp_bonus).map(([count, xp]) => `${count} Orang = Bonus ${xp} XP`).join('\n')}
-`.trim())
-  }
 }
-handler.help = ['ref']
-handler.tags = ['fun']
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <name>.<age>')
+handler.tags = ['xp']
 
-handler.command = ['ref']
-
-handler.register = true
+handler.command = /^(daftar|reg(ister)?)$/i
 
 module.exports = handler
