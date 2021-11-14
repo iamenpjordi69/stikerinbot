d8ed64295cf10e7eed32b16c437696f89e492906
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 18:55:59 +0530

    Update grouplist.js

--- plugins/grouplist.js
+++ plugins/grouplist.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
   let now = new Date() * 1
   let gc = conn.chats.all().filter(v => v.jid.endsWith('g.us'))
-  let txt = gc.map(v => `${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Keluar' : 'Masuk'}]\n${msToDate(db.data.chats[v.jid] === undefined ? '' : db.data.chats[v.jid].expired - now)}`).join`\n\n`
-  conn.reply(m.chat, `Total ${gc.length} Grup\nDaftar Grup:\n${txt}`, m)
+  let txt = gc.map(v => `${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]\n${msToDate(db.data.chats[v.jid] === undefined ? '' : db.data.chats[v.jid].expired - now)}`).join`\n\n`
+  conn.reply(m.chat, `Total ${gc.length} Groups\nGroups List:\n${txt}`, m)
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
 handler.command = /^(group(s|list))$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
@@ -29,6 +29,6 @@ function msToDate(ms) {
   minutes = Math.floor((hoursms) / (60 * 1000));
   minutesms = ms % (60 * 1000);
   sec = Math.floor((minutesms) / (1000));
-  return days + " hari " + hours + " jam " + minutes + " menit";
+  return days + " days " + hours + " hours " + minutes + " minutes";
   // +minutes+":"+sec;
 }
