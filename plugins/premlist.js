17f1ea8f8581086523e0224beb0dacbae16c7dbb
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 12:13:52 +0530

    Update premlist.js

--- plugins/premlist.js
+++ plugins/premlist.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn, isOwner }) => {
   let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `┌〔 Daftar Pengguna Premium 〕` + `\n` + prem.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: prem } })
+  conn.reply(m.chat, `┌〔 Premium Users 〕` + `\n` + prem.map(v => isOwner ? '├ @' + v.replace(/@.+/, '') : '│ ' + conn.getName(v)).join`\n` + '\n└────', m, { contextInfo: { mentionedJid: prem } })
 }
 handler.help = ['premlist']
 handler.tags = ['owner']
