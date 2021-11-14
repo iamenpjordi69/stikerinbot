926ba99232c6f94732545022de6cd4afdf943252
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 07:26:49 +0530

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, isROwner, isOwner, usedPrefix, command }) 
     if (isROwner) global.conn.bye = text
     else if (isOwner) conn.bye = text
     global.db.data.chats[m.chat].sBye = text
-    m.reply('Bye berhasil diatur\n@user (Mention)')
-  } else throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} byebye @user`
+    m.reply('Bye successfully set\n@user (Mention)')
+  } else throw `uhm.. Where is the text?\n\nExample:\n${usedPrefix + command} byebye @user\n\n @user left the group @subject`
 }
-handler.help = ['setbye <teks>']
+handler.help = ['setbye <text>']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setbye$/i
