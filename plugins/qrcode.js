0d53128eb7b2750a7b86e8e818684e827b9fe461
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-14 12:11:56 +0530

    Update qrcode.js

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -3,7 +3,7 @@ let qrcode = require("qrcode")
 let handler = async (m, { conn, text }) => {
   conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '', m)
 }
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 handler.owner = false
