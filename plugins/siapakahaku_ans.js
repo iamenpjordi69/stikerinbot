9525c556e3129fd887e225aae82d9bc740e28575
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-15 10:05:57 +0530

    Update siapakahaku_ans.js

--- plugins/siapakahaku_ans.js
+++ plugins/siapakahaku_ans.js
@@ -3,19 +3,19 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.contentText)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*who/i.test(m.quoted.contentText)) return !0
     this.siapakahaku = this.siapakahaku ? this.siapakahaku : {}
-    if (!(id in this.siapakahaku)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.siapakahaku)) return m.reply('The puzzle has ended')
     if (m.quoted.id == this.siapakahaku[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.siapakahaku[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.who', 'Hint', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.siapakahaku[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.siapakahaku[id][2]} XP`, '© stikerin', 'Siapakah Aku', '.siapaaku')
+            await this.sendButton(m.chat, `✅ *You Guessed it Right !* +${this.siapakahaku[id][2]} XP`, '© MilfBOT', 'Who Am I', '.whoami')
             clearTimeout(this.siapakahaku[id][3])
             delete this.siapakahaku[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*You are Close, Try a little harder!*`)
+        else m.reply(`❌ *Wrong Answer!*`)
     }
     return !0
 }
