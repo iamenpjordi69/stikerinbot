9539a33d4c5b38ffe2b85bedf2303af803a79a56
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-15 17:18:17 +0530

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -4,7 +4,7 @@ async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
     if (id in this.game) {
-        this.sendButton(m.chat, 'There are still unanswered quizzes in this chat', '© MilfBOT', 'GiveUP', 'nyerah', this.game[id].msg)
+        this.sendButton(m.chat, 'There are still unanswered quizzes in this chat', '© MilfBOT', 'Surrender', 'nyerah', this.game[id].msg)
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/iamenpjordi6/679cc869a61daa3aa45f71549c776faf/raw/family100.json')).json()
@@ -20,7 +20,7 @@ There are *${json.jawaban.length}* answers to this question ${json.jawaban.find(
     `.trim()
     this.game[id] = {
         id,
-        msg: await this.sendButton(m.chat, caption, '© MilfBOT', 'GiveUP', 'nyerah', m),
+        msg: await this.sendButton(m.chat, caption, '© MilfBOT', 'Surrender', 'nyerah', m),
         ...json,
         terjawab: Array.from(json.jawaban, () => false),
         winScore,
