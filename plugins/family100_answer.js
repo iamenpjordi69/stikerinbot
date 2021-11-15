abfb72cd6bb8884a78f978e1ce5f7b7bb802a27a
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-15 10:13:40 +0530

    Update family100_answer.js

--- plugins/family100_answer.js
+++ plugins/family100_answer.js
@@ -7,11 +7,11 @@ module.exports = {
         if (!(id in this.game)) return !0
         let room = this.game[id]
         let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
-        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
+        let isSurrender = /^((me)?giveup|surr?ender)$/i.test(m.text)
         if (!isSurrender) {
             let index = room.jawaban.findIndex(v => v.toLowerCase().replace(/[^\w\s\-]+/, '') === text)
             if (index < 0) {
-                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('You are Close, Try a little harder!!')
                 return !0
             }
             if (room.terjawab[index]) return !0
@@ -21,19 +21,19 @@ module.exports = {
         }
         let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
         let caption = `
-*Soal:* ${room.soal}
+*Question:* ${room.soal}
 
-Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
-(beberapa jawaban terdapat spasi)
+There are *${room.jawaban.length}* answers to this question ${room.jawaban.find(v => v.includes(' ')) ? `
+(some answers have spaces)
 `: ''}
-${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
+${isWin ? `*ALL ANSWERS*` : isSurrender ? '*SURRENDER!*' : ''}
 ${Array.from(room.jawaban, (jawaban, index) => {
             return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
         }).filter(v => v).join('\n')}
 
-${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
+${isSurrender ? '' : `+${room.winScore} XP every answer is correct`}
     `.trim()
-        await this.sendButton(m.chat, caption, '© stikerin', `${isWin ? 'Family 100' : isSurrender ? 'Family 100' : 'Nyerah'}`, `${isWin ? ',family100' : isSurrender ? ',family100' : 'nyerah'}`, m, {
+        await this.sendButton(m.chat, caption, '© MilfBOT', `${isWin ? 'Family 100' : isSurrender ? 'Family 100' : 'Give Up'}`, `${isWin ? ',family100' : isSurrender ? ',family100' : 'giveup'}`, m, {
             contextInfo: {
                 mentionedJid: this.parseMention(caption)
             }
