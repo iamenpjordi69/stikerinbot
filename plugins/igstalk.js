c897943ea471526ba4eb3abbc38cf77f47fc1403
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-15 09:33:05 +0530

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Example:\n${usedPrefix + command} nsutjordi`
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} stikerinbot`
 
-  let res = await fetch(global.API('Dehan', '/api/stalk/instagram', { username: args[0] }))
+  let res = await fetch(global.API('zekais', '/igs', { username: args[0] }))
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.profile_pic_url_hd, 'eror.jpg', `*Name:* ${json.data.full_name}\n*Bio:* ${json.data.biography}\n*Followers:* ${json.data.edge_followed_by.count} )
+  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Name:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'No'}\n\nhttp://instagram.com/nsutjordi`, m, 0, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
 }
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
