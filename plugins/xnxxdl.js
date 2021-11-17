3ea60f0469679fa24c954653780ec41b50bc46df
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-17 20:41:56 +0530

    Update xnxxdl.js

--- plugins/xnxxdl.js
+++ plugins/xnxxdl.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, args }) => {
  let res = await fetch(API('Velgrynd', '/api/xnxxdl', { url: args[0] }))
  if (!res.ok) throw await res.text()
  let json = await res.json()
- let { title, low } = json.result
+ let { title, thumb } = json.result
  m.reply(JSON.stringify(json.result, null, 2))
- conn.sendFile(m.chat, low, title, '', m)
+ conn.sendFile(m.chat, thumb, title, '', m)
 handler.help = ['xnxxdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^xnxxdl$/i
