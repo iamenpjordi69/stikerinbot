cbedd8f7ce2b149000f2b81ca579d65340424545
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-18 17:57:14 +0530

    Update _ephemeral.js

--- plugins/_ephemeral.js
+++ plugins/_ephemeral.js
@@ -7,8 +7,8 @@ module.exports = {
       case WA_MESSAGE_STUB_TYPE.CHANGE_EPHEMERAL_SETTING:
         if (chat.detect)
           this.sendMessage(chatUpdate.jid, +m.messageStubParameters[0] ?
-            'Pesan Sementara ON' :
-            'Pesan Sementara OFF'
+            'Message Temporarily ON' :
+            'Message Temporarily OFF'
             , 'extendedTextMessage')
         break
     }
@@ -22,14 +22,14 @@ module.exports = {
                 content: m.msg,
                 sender: m.sender
               }
-              this.sendMessage(m.chat, ('*BUG GRUP TERDETEKSI, JANGAN SCROLL KEATAS! HAPUS CHAT INI BIAR GA EROR!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
+              this.sendMessage(m.chat, ('*BUG / VIRUS DETECTED, DON"T SCROLL UP! DELETE THIS CHAT SO THERE WILL BE NO ERROR!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
               // this.modifyChat(m.chat, 'clear', {
               //     includeStarred: false
               // }).catch(console.error)
               this.reply(global.owner[0] + '@s.whatsapp.net', `
-Pelaku pengirim bug gc @${m.sender.split`@`[0]}
+The number of the gc bug sender @${m.sender.split`@`[0]}
 ID: ${m.isGroup ? m.chat : m.sender}
-Nama: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
+Name: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
 `.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
             }
             break
