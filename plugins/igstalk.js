8c9a5fa4594f348469ff58cb6284d12c8f1aa7c1
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-21 23:19:48 +0530

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,14 +1,18 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `Example:\n${usedPrefix + command} __a_n_i_r_u_d_h_`
+  if (!args[0]) throw `Example:\n${usedPrefix + command} nsutjordi`
 
   let res = await fetch(`https://fxc7-api.herokuapp.com/api/stalk/ig?apikey=pnj8NAJb&username=${args}`)
-  if (!res.ok) throw eror
+   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { full_name, biography, followers, following, posts_count, is_private, is_verified, profile_url } = json.result
-  if (json.status != 200) throw json
-  conn.sendFile(m.chat, profile_url, 'eror.jpg', `*Name:* ${full_name}\n*Bio:* ${biography}\n*Followers:* ${followers}\n*Following:* ${following}\n*Posts:* ${posts_count}\n*Private:* ${is_private}\n*Verified:* ${is_verified}\n\nhttps://www.instagram.com/nsutjordi`, m, 0 )
-}
+  if(!json.result) throw json
+  let { full_name, username} = json.result
+let spotifyinfo = `Name : ${full_name}
+Username : ${username}
+`
+
+  await conn.sendFile(m.chat, thumb, '', spotifyinfo, m)
+}  
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
 handler.command = /^(igstalk)$/i
