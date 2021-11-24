789ff52bef6c047820852e6ebe1c68f373bb7f72
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-24 10:51:49 +0530

    Update jadian-cek.js

--- plugins/jadian-cek.js
+++ plugins/jadian-cek.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
 
   if (!text && !m.quoted){
     user = m.sender
-    orang = "Kamu"
+    orang = "you"
   }else if(text) {
     var user = number + '@s.whatsapp.net'
     orang = "The person you tagged"
@@ -36,9 +36,9 @@ let handler = async (m, { conn, usedPrefix, text }) => {
   }
 
   if (global.db.data.users[user].pasangan == "") {
-    conn.reply(m.chat, `${orang} no partner and not shooting anyone\n\n*Type .jadian @user to shoot someone*`, m)
+    conn.reply(m.chat, `${orang} has no partner and not shooting anyone\n\n*Type .jadian @user to shoot someone*`, m)
   }else if (global.db.data.users[global.db.data.users[user].pasangan].pasangan != user){
-    conn.reply(m.chat, `${orang} waiting for an answer from @${global.db.data.users[user].pasangan.split('@')[0]} because it is not accepted or rejected\n\nType *${usedPrefix}ikhlasin* to let go!`, m,{contextInfo: {
+    conn.reply(m.chat, `${orang} waiting for an answer from @${global.db.data.users[user].pasangan.split('@')[0]} because it is not accepted or rejected\n\nType *${usedPrefix}sincere* to let go!`, m,{contextInfo: {
       mentionedJid: [global.db.data.users[user].pasangan]
     }})
   }else {
@@ -47,8 +47,8 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     }})
   }
 }
-handler.help = ['cekpacar']
+handler.help = ['checkstatus']
 handler.tags = ['jadian']
-handler.command = /^(cekpacar)$/i
+handler.command = /^(checkstatus)$/i
 handler.fail = null
 module.exports = handler
