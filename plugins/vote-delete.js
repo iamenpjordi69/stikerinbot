ff308142fd1a504feaed8dbf8b533f555c12ae3a
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-24 09:38:03 +0530

    Update vote-delete.js

--- plugins/vote-delete.js
+++ plugins/vote-delete.js
@@ -2,16 +2,16 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
     if (!(id in conn.vote)) {
-        await conn.sendButton(m.chat, `_*tidak ada voting digrup ini!*_`, '© stikerin', 'MULAI VOTE', `${usedPrefix}mulaivote`, m)
+        await conn.sendButton(m.chat, `_*No voting in this group!*_`, '© MilfBOT', 'START VOTE', `${usedPrefix}startvote`, m)
         throw false
     }
     delete conn.vote[id]
-    m.reply(`Berhasil!`)
+    m.reply(`Voting Ended Successfully!`)
 
 }
-handler.help = ['hapusvote']
+handler.help = ['unvote']
 handler.tags = ['vote']
-handler.command = /^(delete|hapus)vote$/i
+handler.command = /^(end|un)vote$/i
 handler.group = true
 handler.admin = true
 module.exports = handler
