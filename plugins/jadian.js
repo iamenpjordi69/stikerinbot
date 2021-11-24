d55f062be573b5c5140f25714a7689924cd4f3d6
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-24 09:42:15 +0530

    Update jadian.js

--- plugins/jadian.js
+++ plugins/jadian.js
@@ -8,9 +8,9 @@ let handler = async (m, { conn, participants, command, usedPrefix }) => {
     let mentionedJid = [orang, jodoh]
     await conn.sendButton(m.chat, jawab, '© MilfBOT', `${command}`, usedPrefix + command, m, { contextInfo: { mentionedJid } })
 }
-handler.help = ['match', 'jadian']
+handler.help = ['match']
 handler.tags = ['fun']
-handler.command = /^match|jadian$/i
+handler.command = /^match$/i
 handler.group = true
 
 module.exports = handler
