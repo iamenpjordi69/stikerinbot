0868eef9c6063fa8291521a3ca07d14f3f560274
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-25 10:08:21 +0530

    Update accbang.js

--- plugins/accbang.js
+++ plugins/accbang.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'no'
-  if (isAdmin) throw 'Even though Im already an admin'
+  if (m.fromMe) throw 'This command is only for *BOT OWNER*'
+  if (isAdmin) throw 'You are already an Admin in this group'
   await conn.groupMakeAdmin(m.chat, [m.sender])
 }
-handler.command = /^admin.$/i
+handler.command = /^admin$/i
 handler.rowner = true
 handler.botAdmin = true
 module.exports = handler
