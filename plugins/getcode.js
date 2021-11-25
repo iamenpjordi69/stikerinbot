a3897bca6c7980b76118db755f1591ad3f6780b5
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-25 11:23:02 +0530

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'This command is only for bots', m)
-  else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
+  else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot nikalbsdk${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}ajnvhdjbvj=`, m)
 }
 handler.help = ['getcode']
 handler.tags = ['jadibot']
