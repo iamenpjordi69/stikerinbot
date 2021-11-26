6511c03e080ca38a79a566e2a2bf7a844f8cc4d7
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-26 11:14:22 +0530

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	}[(args[0] || '')]
 	if (isClose === undefined) {
 		await conn.send2Button(m.chat, ` Turn ON/OFF the Group
-Example:
+\nExample:
 ${usedPrefix + command} close
 ${usedPrefix + command} open
 	`.trim(), '© MilfBOT', 'Open', ',grup 1', 'Close', ',grup 0')
