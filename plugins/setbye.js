7d42eef7c8b3dc91e8f5877d7c45bca6b36b0a54
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-27 12:03:37 +0530

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, isROwner, isOwner, usedPrefix, command }) 
     if (isROwner) global.conn.bye = text
     else if (isOwner) conn.bye = text
     global.db.data.chats[m.chat].sBye = text
-    m.reply('Bye successfully set\n@user (Mention)')
+    m.reply('Bye successfully set\n@user (Mention)\n@subject(Group Title)')
   } else throw `uhm.. Where is the text?\n\nExample:\n${usedPrefix + command} byebye @user\n\n @user left the group @subject`
 }
 handler.help = ['setbye <text>']
