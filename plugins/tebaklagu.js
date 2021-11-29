2f963cd9089050d213d7789e588f9b1917fac02f
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-29 12:27:10 +0530

    Update tebaklagu.js

--- plugins/tebaklagu.js
+++ plugins/tebaklagu.js
@@ -6,33 +6,33 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tebaklagu = conn.tebaklagu ? conn.tebaklagu : {}
     let id = m.chat
     if (id in conn.tebaklagu) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklagu[id][0])
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebaklagu[id][0])
         throw false
     }
     // ubah isi 'id' kalo mau ganti playlist spotifynya
-    let res = await fetch(global.API('xteam', '/game/tebaklagu/', { id: '3AaKHE9ZMMEdyRadsg8rcy' }, 'APIKEY'))
+    let res = await fetch(global.API('mel', '/game/tebaklagu/', { id: '3AaKHE9ZMMEdyRadsg8rcy' }, 'apikey'))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let result = await res.json()
     let json = result.result
     if (!result.status) throw json
     let caption = `
-TEBAK JUDUL LAGU
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik *${usedPrefix}cek* untuk bantuan
+GUESS THE SONG TITLE
+Timeout *${(timeout / 1000).toFixed(2)} seconds*
+Type *${usedPrefix}check*for help
 Bonus: ${poin} XP
-*Balas pesan ini untuk menjawab!*`.trim()
+*Reply to this message to answer!*`.trim()
     conn.tebaklagu[id] = [
         await m.reply(caption),
         json, poin,
         setTimeout(async () => {
-            if (conn.tebaklagu[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.judul}*`, '© stikerin', 'Tebak Lirik', `.tebaklirik`, conn.tebaklagu[id][0])
+            if (conn.tebaklagu[id]) await conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.judul}*`, '© MilfBOT', 'Guess the Lyrics', `.tebaklirik`, conn.tebaklagu[id][0])
             delete conn.tebaklagu[id]
         }, timeout)
     ]
     await conn.sendFile(m.chat, json.preview, 'eror.mp3', '', m, 1, { mimetype: 'audio/mp4' })
 }
-handler.help = ['tebaklagu']
+handler.help = ['guessthesong']
 handler.tags = ['game']
-handler.command = /^tebaklagu$/i
+handler.command = /^(guessthesong|gts)$/i
 
 module.exports = handler
