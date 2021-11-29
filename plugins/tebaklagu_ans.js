6f5e104fe8d78e7922cddd1eb030a92c724fceab
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-29 12:30:38 +0530

    Update tebaklagu_ans.js

--- plugins/tebaklagu_ans.js
+++ plugins/tebaklagu_ans.js
@@ -5,17 +5,17 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/TEBAK JUDUL LAGU/i.test(m.quoted.text)) return !0
     this.tebaklagu = this.tebaklagu ? this.tebaklagu : {}
-    if (!(id in this.tebaklagu)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.tebaklagu)) return m.reply('Guess the Song game has ended')
     if (m.quoted.id == this.tebaklagu[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
-        if (/^.*cek$/i.test(m.text)) return !0
+        if (/^.*check$/i.test(m.text)) return !0
         if (m.text.toLowerCase() == json.judul.toLowerCase()) {
             global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.tebaklagu[id][2]} XP`, '© stikerin', 'Tebak Lagu', '.tebaklagu', m)
+            await this.sendButton(m.chat, `*You Guessed Right ✅!* +${this.tebaklagu[id][2]} XP`, '© MilfBOT', 'Guess The Song', '.guessthesong', m)
             clearTimeout(this.tebaklagu[id][3])
             delete this.tebaklagu[id]
-        } else if (similarity(m.text.toLowerCase(), json.judul.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (similarity(m.text.toLowerCase(), json.judul.toLowerCase().trim()) >= threshold) m.reply(`*You are close , try a little more!*`)
+        else m.reply(`*Wrong Guess ❌!*`)
     }
     return !0
 }
