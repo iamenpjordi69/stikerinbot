2499217d24c1b0b1d8f8707e22541240cbb45cf1
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-30 19:22:12 +0530

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -3,10 +3,8 @@ let handler = m => m
 
 handler.all = async function (m, { isBlocked }) {
 
-    if (isBlocked) return
-    if (m.isBaileys) return
-    if (m.chat.endsWith('broadcast')) return
-    let setting = db.data.settings[this.user.jid]
+    if (isBlocked || m.fromMe || m.chat.endsWith('broadcast')) return
+    let set = db.data.settings[this.user.jid]
     let { isBanned } = db.data.chats[m.chat]
     let { banned } = db.data.users[m.sender]
 
@@ -14,8 +12,8 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'MilfBOT is not Active' : banned ? 'You are banned' : 'MilfBOT here',
-                '© MilfBOT',
+                isBanned ? 'MilfBOT is Active' : banned ? 'You are banned' : 'MilfBOT is Active',
+                '© stikerin',
                 isBanned ? 'Unban' : banned ? 'Bot Owner' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
                 m.isGroup ? 'Ban' : isBanned ? 'Unban' : 'Donate',
@@ -27,9 +25,9 @@ handler.all = async function (m, { isBlocked }) {
 
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Open this link')) && !m.isBaileys && !m.isGroup) {
-        this.sendButton(m.chat, `┌〔 Invite Bot to Group 〕
-├ Send Owner Group Link
-├ Send invitation link to Bot
+        this.sendButton(m.chat, `┌「 Add Bots to your Group 」
+├ Invite Bot via Invitation
+├ Send Group Link to Owner
 └────
 
 https://github.com/iamenpjordi
@@ -37,15 +35,15 @@ https://github.com/iamenpjordi
     }
 
     // salam
-    let reg = /(hello|Hello)/i
+    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
     let isSalam = reg.exec(m.text)
     if (isSalam && !m.fromMe) {
-        m.reply(`Hi, How are you?\n _Have a Nice Day_`)
+        m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
     }
 
     // backup db
-    if (setting.backup) {
-        if (new Date() * 1 - setting.backupDB > 1000 * 60 * 60) {
+    if (set.backup) {
+        if (new Date() * 1 - set.backupTime > 1000 * 60 * 60) {
             let d = new Date
             let date = d.toLocaleDateString('id', {
                 day: 'numeric',
@@ -55,16 +53,18 @@ https://github.com/iamenpjordi
             await global.db.write()
             this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
             this.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
-            setting.backupDB = new Date() * 1
+            set.backupTime = new Date() * 1
         }
     }
 
     // update status
-    if (new Date() * 1 - setting.status > 1000) {
-        let _uptime = process.uptime() * 1000
-        let uptime = clockString(_uptime)
-        await this.setStatus(`Active since ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Group Only' : 'Public'} | MilfBOT by JORDI`).catch(_ => _)
-        setting.status = new Date() * 1
+    if (set.autoupdatestatus) {
+        if (new Date() * 1 - set.status > 1000) {
+            let _uptime = process.uptime() * 1000
+            let uptime = clockString(_uptime)
+            await this.setStatus(`Active Since ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Group only' : 'Public'} | MilfBOT by Jordi`).catch(_ => _)
+            set.status = new Date() * 1
+        }
     }
 
 }
@@ -76,8 +76,4 @@ function clockString(ms) {
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
     return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
 }
