f9cd1024e41ebac5614294b4ec9e14062dee498e
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-11-30 19:27:39 +0530

    Update _ephemeral.js

--- plugins/_ephemeral.js
+++ plugins/_ephemeral.js
@@ -2,13 +2,14 @@ const { WA_MESSAGE_STUB_TYPE } = require('@adiwajshing/baileys')
 
 module.exports = {
   all(m, chatUpdate) {
+    if (!db.data.settings[this.user.jid].buggc) return
     let chat = global.db.data.chats[chatUpdate.jid]
     switch (m.messageStubType) {
       case WA_MESSAGE_STUB_TYPE.CHANGE_EPHEMERAL_SETTING:
         if (chat.detect)
           this.sendMessage(chatUpdate.jid, +m.messageStubParameters[0] ?
-            'Message Temporarily ON' :
-            'Message Temporarily OFF'
+            'Order Temporarily ON' :
+            'Order Temporarily OFF'
             , 'extendedTextMessage')
         break
     }
@@ -22,12 +23,12 @@ module.exports = {
                 content: m.msg,
                 sender: m.sender
               }
-              this.sendMessage(m.chat, ('*BUG / VIRUS DETECTED, DON"T SCROLL UP! DELETE THIS CHAT SO THERE WILL BE NO ERROR!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
+              this.sendMessage(m.chat, ('*GROUP BUG DETECTED, DONT SCROLL UP! DELETE THIS CHAT SO THERE WILL BE NO ERROR!!!*\n\n' + require('util').format(log)).padEnd(65536, '\n'), 'extendedTextMessage')
               // this.modifyChat(m.chat, 'clear', {
               //     includeStarred: false
               // }).catch(console.error)
               this.reply(global.owner[0] + '@s.whatsapp.net', `
-The number of the gc bug sender @${m.sender.split`@`[0]}
+The Sender of the group bug sender @${m.sender.split`@`[0]}
 ID: ${m.isGroup ? m.chat : m.sender}
 Name: ${m.isGroup ? this.getName(m.chat) : this.getName(m.sender)}
 `.trim(), null, { contextInfo: { mentionedJid: [m.sender] } })
