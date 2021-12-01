9b0664800a751e14e64737ace585009d33c34759
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-01 10:04:07 +0530

    Update animalfact.js

--- plugins/animalfact.js
+++ plugins/animalfact.js
@@ -1,33 +1,26 @@
-let fetch = require("node-fetch");
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+  let ar = ['dog', 'cat', 'panda', 'fox', 'red_panda', 'koala', 'bird', 'raccoon', 'kangaroo']
   if (!text)
     throw `
-*${usedPrefix}${command} <animal name>*
-Example:
-*${usedPrefix}${command} <dog>*\n
-┌〔 Options 〕
-├ dog
-├ cat
-├ panda
-├ fox
-├ red_panda
-├ koala
-├ birb
-├ raccoon
-├ kangaroo
+┌「 Choices 」
+${ar.map(v => '├ ' + v).join`\n`}
 └────
-`.trim();
-  let res = await fetch(
-    API("https://some-random-api.ml", "/animal/" + text, {})
-  );
-  if (!res.ok) throw `${res.status} ${res.statusText}`;
-  let json = await res.json();
-  if (json.image) await conn.sendFile(m.chat, json.image, "", `${json.fact}\n\n~fatur`, m);
-  else throw json;
-};
-handler.help = ["animal"].map((v) => v + " <hewan>");
-handler.tags = ["internet"];
-handler.command = /^(animal|animalfact)$/i;
 
-module.exports = handler;
+Example:
+${usedPrefix}${command} panda
+`.trim()
+  let res = await fetch(
+    API('https://some-random-api.ml', '/animal/' + text, {})
+  )
+  if (!res.ok) throw `${res.status} ${res.statusText}`
+  let json = await res.json()
+  if (json.image) await conn.sendFile(m.chat, json.image, '', `${json.fact}\n\n© MilfBOT`, m)
+  else throw json
+}
+handler.help = ['animal'].map((v) => v + ' <name>')
+handler.tags = ['internet']
+handler.command = /^(animal|animalfact)$/i
+
+module.exports = handler
