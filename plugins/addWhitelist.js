9dd0b25d07722779066d7a01a4982dc2d1ea3314
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-01 09:51:11 +0530

    Update addWhitelist.js

--- plugins/addWhitelist.js
+++ plugins/addWhitelist.js
@@ -1,9 +1,9 @@
 let handler = async (m, { usedPrefix, command, text, args }) => {
     if (!args || !['add', 'remove'].includes(args[0].toLowerCase())) throw `
-*Penggunaan:* ${usedPrefix + command} <add|remove> nomor,nomor,...,nomor
-*Contoh:*
-${usedPrefix + command} add 6281111111111,12345678901,0
-${usedPrefix + command} remove 6281111111111,12345678901,0
+*Use:* ${usedPrefix + command} <add|remove> number,number,...,number
+*Example:*
+${usedPrefix + command} add 917428849575,19715122809,0
+${usedPrefix + command} remove 917428849575,19715122809,0
 `.trim()
     let type = args[0].toLowerCase() === 'add' ? true : false
     let teks = text.replace(args[0], '').trim()
@@ -13,11 +13,12 @@ ${usedPrefix + command} remove 6281111111111,12345678901,0
         if (!user) user = global.db.data.users[who] = {}
         user.whitelist = type
     }
-    m.reply(`Berhasil ${type ? 'menambah' : 'menghapus'} whitelist ${users.length} pengguna`)
+    m.reply(`Successfully ${type ? 'added' : 'removed'} ${users.length} users as whitelist`)
 }
-handler.help = ['whitelist'].map(v => v + ' nomor,nomor')
+handler.help = ['whitelist'].map(v => v + ' number,number')
 handler.tags = ['owner']
 handler.command = ['whitelist']
+
 handler.owner = true
 
 module.exports = handler
