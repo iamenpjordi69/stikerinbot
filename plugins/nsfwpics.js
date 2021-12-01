f0bb72ef80f8159268c754cf09431960429355f0
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-01 21:19:38 +0530

    Update nsfwpics.js

--- plugins/nsfwpics.js
+++ plugins/nsfwpics.js
@@ -1,8 +1,7 @@
-let fetch = require("node-fetch")
 let handler = async (m, { conn, command }) => {
   if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
   m.reply('Loading...')
-  let res = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/${command}?apikey=apirey`)
+  let res = `https://server-api-rey.herokuapp.com/api/nsfw/${command}?apikey=apirey`
   conn.sendFile(m.chat, res, 'milf.jpg', '© MilfBOT', m)
 }
 handler.help = ["ass", "bdsm", "blowjob", "cuckold", "cum", "ero", "femdom", "foot", "gangbang", "glasses", "hentai", "jahy", "masturbation", "orgy", "panties", "pussy", "tentacles", "thighs", "yuri", "zettai"]
