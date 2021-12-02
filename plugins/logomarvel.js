35bc1b1a34bd34579bd401dabd4e6e0ff4c39b9d
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-02 07:41:53 +0530

    Update logomarvel.js

--- plugins/logomarvel.js
+++ plugins/logomarvel.js
@@ -1,10 +1,10 @@
  let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('*[ ❗ ] Wait,Proses...*')
+  if (!args[0]) throw 'Please provide text seprated by |'
+  m.reply('*[ ❗ ] Wait,Processing...*')
   let res = `https://api.zeks.xyz/api/marvellogo?text1=${response[0]}&text2=${response[1]}&apikey=apivinz`
-  conn.sendFile(m.chat, res, 'marvellogo.jpg', `Nih kak`, m, false)
+  conn.sendFile(m.chat, res, 'marvellogo.jpg', `@ MilfBOT`, m, false)
 }
 handler.help = ['marvellogo'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
