bb32de6410f48ab3aa62a06db68e049dcf8aca7f
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-06 14:36:39 +0530

    Create cutvideo.js

---
+++ plugins/cutvideo.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+try {
+       if (!args[0]) return m.reply('enter numbers')
+       if (!args[1]) return m.reply('enter numbers again')
+       
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/video/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let out = Buffer.alloc(0)
+            let ran = getRandom('.mp4')
+          exec(`ffmpeg -ss ${args[0]} -i ${media} -to ${args[1]} -c copy ${ran}`, (err, stderr, stdout) => {
+          fs.unlinkSync(media)
+          if (err) m.reply(`_*Error!*_`)
+          let buff = fs.readFileSync(ran)
+        conn.sendFile(m.chat, buff, ran, 'successfully trimmed video🎥', m, 0, { thumbnail: Buffer.alloc(0)})
+       fs.unlinkSync(ran)
+            })
+       } else m.reply(`Reply to the video that you want to trim the video minutes with the caption *${usedPrefix}cutvideo reply 00:00:01*`)
+     } catch (err) {
+     m.reply(`${err}`)
+    }
+}
+
+
+handler.help = ['cutvideo']
+handler.tags = ['convert']
+handler.command = /^(cutvideo)$/i
+
+
+handler.fail = null
+
+module.exports = handler
+
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
+}
