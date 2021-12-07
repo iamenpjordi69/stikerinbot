e106299586bcee35dba69841116872566f043946
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-07 17:10:42 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,18 +1,15 @@
-handler.limit = true
-module.exports = handler
-
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. Send Tiktok URL?\n\nExample:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
-  if (!args[0].match(/tiktok/gi)) throw `URL Invalid!!`
+  if (!args[0]) throw `uhm..Send tiktok url?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
+  if (!args[0].match(/tiktok/gi)) throw `url salah`
 
-  let res = await fetch(API('rey', '/api/download/tiktok', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('mel', '/tiktok', { url: args[0] }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
-  // if (!json.status) throw json
+  if (!json.status) throw json
   await m.reply(wait)
-  await conn.sendFile(m.chat, json.result.nowatermark, '', `© MilfBOT`, m)
+  await conn.sendFile(m.chat, json.result.link, '', `${json.result.caption}\n\n© stikerin`, m)
 
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
