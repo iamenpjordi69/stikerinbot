e1fb2c990d03a6fc841b6231acd71120bd24cc27
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-11 08:25:43 +0530

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -2,22 +2,22 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `Example:\n${usedPrefix + command} nsutjordi`
 
-  let res = await fetch(`https://zenzapi.xyz/api/stalker/ig?username=${args}&apikey=zacros`)
+  let res = await fetch(`https://fxc7-api.herokuapp.com/api/stalk/ig?apikey=pnj8NAJb&username=${args}`)
    if (!res.ok) throw await res.text()
   let json = await res.json()
   if(!json.result) throw json
-  let { full_name, user_name, followers, following, user_id, private, verified, biography, profile_hd} = json.result
+  let { full_name, username, followers, following, posts_count, is_private, is_verified, biography, profile_url} = json.result
 let spotifyinfo = `🎯 *Name:* ${full_name}
-👥 *Following:* ${following}
-👩‍👩‍👧‍👧 *Followers:* ${followers}
-📮 *User ID:* ${user_id}
-🔒 *Private Account:* ${private} 
+👥 *Following:* ${followers}
+👩‍👩‍👧‍👧 *Followers:* ${following}
+📮 *Total Post:* ${posts_count}
+🔒 *Account Type:* ${is_private} 
 
 📜 *Bio:* ${biography}
-🔗 *Profile URL:* https://www.instagram.com/${user_name}
+🔗 *Profile URL:* https://www.instagram.com/${username}
 `
 
-  await conn.sendFile(m.chat, profile_hd, '', spotifyinfo, m)
+  await conn.sendFile(m.chat, profile_url, '', spotifyinfo, m)
 }  
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
