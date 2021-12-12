a60eddb1e20e168c229135bdbfd48e15673e0f7a
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-12 14:53:21 +0530

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*Effects List*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*Effects List*\n\nExample: .textpro neon text' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Effect *${effect}* not found`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)
