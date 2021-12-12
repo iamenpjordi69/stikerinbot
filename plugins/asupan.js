377845e3e1605fe56435d96e0d87e475b49fed51
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-12 18:40:08 +0530

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,12 +1,5 @@
-let fetch = require("node-fetch")
-let arr = []
 let handler = async (m, { conn }) => {
-  let res = await fetch('http://sansekai.my.id/sansekai.txt')
-  if (!res.ok) throw `${res.status} ${res.statusText}`
-let txt = await res.text()
-arr = txt.split`\n`
-  let asupan = arr[Math.floor(Math.random() * arr.length)]
-  await conn.sendFile(m.chat, 'http://sansekai.my.id/ptl_repost/'+asupan, asupan, null, m)
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
