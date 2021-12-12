9bcd7ec52cc51b8271ea71984062fa23e93179d9
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-12 18:56:10 +0530

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -5,7 +5,7 @@ let res = await fetch('https://gist.githubusercontent.com/iamenpjordi6/398997898
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Joke', '© MilfBOT', 'Get Again', '/joke', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Joke', '© MilfBOT', 'Get Again', '/meme', m)
 }
 handler.command = /^(meme)$/i
 handler.tags = ['fun']
