7b593ea14caf718e2da770d660787608652ef225
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-17 11:26:18 +0530

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -2,18 +2,22 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `Example:\n${usedPrefix + command} nsutjordi`
 
-  let res = await fetch(`https://fxc7-api.herokuapp.com/api/stalk/ig?apikey=pnj8NAJb&username=${args}`)
+  let res = await fetch('https://upgrade/api/instagram/stalk?user=nsutjordi', {
+    headers: {
+        'accept': 'application/json'
+    }
+    });
    if (!res.ok) throw await res.text()
   let json = await res.json()
   if(!json.result) throw json
-  let { full_name, username, followers, following, posts_count, is_private, is_verified, biography, profile_url} = json.result
-let spotifyinfo = `🎯 *Name:* ${full_name}
-👥 *Following:* ${followers}
-👩‍👩‍👧‍👧 *Followers:* ${following}
-📮 *Total Post:* ${posts_count}
-🔒 *Account Type:* ${is_private} 
+  let { name, username, follower_count, following_count, media_count, private, verified, bio, profile_url} = json.result
+let spotifyinfo = `🎯 *Name:* ${name}
+👥 *Following:* ${follower_count}
+👩‍👩‍👧‍👧 *Followers:* ${following_count}
+📮 *Total Post:* ${media_count}
+🔒 *Account Type:* ${private} 
 
-📜 *Bio:* ${biography}
+📜 *Bio:* ${bio}
 🔗 *Profile URL:* https://www.instagram.com/${username}
 `
 
