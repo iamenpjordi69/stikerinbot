00077d3fff8e7397c73991df3060ef1c8504308f
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-19 09:01:38 +0530

    Update tebakkata.js

--- plugins/tebakkata.js
+++ plugins/tebakkata.js
@@ -5,31 +5,31 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
     if (id in conn.tebakkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakkata[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')
+    let res = await fetch('https://gist.githubusercontent.com/iamenpjordi6/673ebb6ff53ab40fbddf43538e7a7e2c/raw/guesstheword.json')
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let data = await res.json()
     let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
-${json.soal}
+${json.question}
 
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}teka untuk bantuan
+Timeout *${(timeout / 1000).toFixed(2)} seconds
+Type ${usedPrefix}teka for help
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkata[id] = [
-        await conn.sendButton(m.chat, caption, '© stikerin', 'Bantuan', '.teka', m),
+        await conn.sendButton(m.chat, caption, '© MilfBOT', 'HINT', '.teka', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tebakkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Tebak Kata', '.tebakkata', conn.tebakkata[id][0])
+            if (conn.tebakkata[id]) await conn.sendButton(m.chat, `Time is over !!\nThe answer is *${json.answer}*`, '© MilfBOT', 'Guess the Word', '.guesstheword', conn.tebakkata[id][0])
             delete conn.tebakkata[id]
         }, timeout)
     ]
 }
-handler.help = ['tebakkata']
+handler.help = ['guesstheword']
 handler.tags = ['game']
-handler.command = /^tebakkata/i
+handler.command = /^guesstheword
 
 module.exports = handler
