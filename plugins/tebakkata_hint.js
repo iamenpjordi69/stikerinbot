a725042bf3fc37a44958cb96accc165c54d2a4e9
iamenpjordi69 <89603788+iamenpjordi69@users.noreply.github.com>
2021-12-19 09:01:44 +0530

    Update tebakkata_hint.js

--- plugins/tebakkata_hint.js
+++ plugins/tebakkata_hint.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakkata)) throw false
     let json = conn.tebakkata[id][1]
-    let ans = json.jawaban.trim()
+    let ans = json.answer.trim()
     let clue = ans.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebakkata[id][0])
+    conn.reply(m.chat, '```' + clue + '```\nReply to the question, not this message', conn.tebakkata[id][0])
 }
 handler.command = /^teka$/i
 handler.limit = true
